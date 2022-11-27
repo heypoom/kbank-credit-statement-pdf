310976d5ed49caa7a18313aa5056b667fddfdaa4
Phoomparin Mano <poom@hey.com>
2022-11-28 00:10:10 +0700

    chore: output

--- src/main.ts
+++ src/main.ts
@@ -4,54 +4,68 @@ import * as fs from 'fs/promises'
 const isDate = (s: string) => /\d{2}\/\d{2}\/\d{2}/.test(s)
 
 function processLineItem(contents: string[]): string[][] {
-    const startIndex = contents.findIndex(isDate)
+  const startIndex = contents.findIndex(isDate)
 
-    const lines = contents.slice(startIndex)
-    let groups: string[][] = []
+  const lines = contents.slice(startIndex)
+  let groups: string[][] = []
 
-    let p = 0
-    let tx = 0
-    
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i]
+  let p = 0
+  let tx = 0
 
-        // Start transaction processing
-        if (isDate(line)) {
-            if (p === 2) {
-                p = 1
-                tx += 1
-            } else {
-                p += 1
-            }
-        }
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
 
-        if (!groups[tx]) groups[tx] = []
-
-        // Transaction Processing
-        groups[tx].push(line)
-    
+    // Start transaction processing
+    if (isDate(line)) {
+      if (p === 2) {
+        p = 1
+        tx += 1
+      } else {
+        p += 1
+      }
     }
-    
-    groups[tx] = groups[tx].slice(0, 5)
 
-    return groups
+    if (!groups[tx]) groups[tx] = []
+
+    // Transaction Processing
+    groups[tx].push(line)
+  }
+
+  if (groups[tx]) {
+    groups[tx] = groups[tx].slice(0, 5)
+  }
+
+  return groups
+}
+
+async function parse(key: string) {
+  const extractor = new PDFExtract()
+  const file = await extractor.extract(`./input/${key}.pdf`)
+
+  console.log(`${key} has ${file.pages.length} pages`)
+
+  const transactions = file.pages.flatMap((page) => {
+    const contents = page.content.map((c) => c.str.trim()).filter((c) => c)
+
+    return processLineItem(contents)
+  })
+
+  console.log(`${key} has ${transactions.length} transactions.`)
+
+  const csv = transactions
+    .map((t) => t.map((x) => `"${x}"`).join(','))
+    .join('\n')
+
+  await fs.writeFile(`./output/${key}.csv`, csv)
 }
 
 async function main() {
-    const extractor = new PDFExtract()
-    const file = await extractor.extract('./input/in.pdf')
-    console.log(file.pages.length)
+  const fileNames = await fs.readdir('./input')
+  const inputs = fileNames.map((k) => k.replace('.pdf', ''))
 
-    const transactions = file.pages.slice(2).flatMap(page => {
-        const contents = page.content.map(c => c.str.trim()).filter(c => c)
+  await Promise.all(inputs.map(parse))
 
-        return processLineItem(contents)
-    })
-    
-    console.log(transactions.length)
-    
-    const csv = transactions.map(t => t.map(x => `"${x}"`).join(',')).join('\n')
-    await fs.writeFile('./tx.csv', csv)
+  console.log('Done')
 }
 
 main()
